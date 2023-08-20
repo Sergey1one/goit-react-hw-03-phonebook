897a7bcaa861d2bb805fab20bf66a8962adad733
Sergey1one <sm.bydin@gmail.com>
2023-08-20 13:52:21 +0300

    Add new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,17 +11,30 @@ import ContactForm from './ContactForm'
 export class App extends Component  {
   state = {
     contacts: [
-     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    
     ],
     filter:''
   
   }
   
- 
-  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+   
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts })
+    }
+ }
+
+  componentDidUpdate(prevProps, prevState) {
+    const {contacts}=this.state
+    console.log(prevState)
+    console.log(this.state)
+    if (prevState.contacts !== contacts) {
+      console.log("NRW");
+      localStorage.setItem('contacts',JSON.stringify(contacts))
+    }
+  }
   formSubmitHandler = ({ id, name, number }) => {
     const contact = { id, name, number };
     this.setState(({ contacts }) => {
@@ -52,7 +65,7 @@ export class App extends Component  {
       
       <Box width='400px' pl='16px'>
         <Section title={'Phonebook'}>
-        
+                 
           <ContactForm onSubmit={this.formSubmitHandler} contacts={ contacts} />
         </Section>
         <Section title="Contacts">
